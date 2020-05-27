d2e600c2568a8ede3507ff6aac8aa0a6bb16dec2
shijir0927 <shijiree_mng@yahoo.com>
2020-05-26 17:26:22 -0700

    is unique solution

---
+++ Chapter_1_Arrays_and_Strings/1.1_Is_Unique.cpp
@@ -0,0 +1,41 @@
+/*Implement an algorigthm to determine if a string has all unique
+charachters. What if you can't use additional data structure?*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+/*Solution:
+Creating a vector of booleans and setting true for 
+the characters we get. If the bool value is already true,
+that means there's a duplicate character.*/
+bool isUnique(string s)
+{
+    if (s.length() > 128)
+        return false;
+
+    vector<bool> characters(128);
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        int value = s[i];
+
+        if (characters[value])
+        {
+            return false;
+        }
+
+        characters[value] = true;
+    }
+    return true;
+}
+
+int main()
+{
+    string word;
+    cout << "Please enter a string:";
+    cin >> word;
+
+    isUnique(word) ? cout << "Unique!" << endl : cout << "Not unique!" << endl;
+
+    return 0;
+}
